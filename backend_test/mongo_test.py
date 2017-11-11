fdcafa5ce5ba9a0c940e02434700fba7982c43d3
kaichimomose <30785433+kaichimomose@users.noreply.github.com>
2017-11-11 13:20:10 -0800

    patch successful

--- backend_test/mongo_test.py
+++ backend_test/mongo_test.py
@@ -1,7 +1,7 @@
 import pdb
 import json
 from flask import Flask, request
-from pymongo import MongoClient
+from pymongo import MongoClient, ReturnDocument
 # from bson import Binary, Code
 from bson.json_util import dumps
 from util import JSONEncoder
@@ -69,15 +69,65 @@ def get_or_post_courses():
                 {'number': number}
             )
         response_json = dumps(result)
-        pdb.set_trace()
         if response_json == 'null':
-            return("Error", 400, None)
+            return("Error: There is no class assgined number:{}".format(number), 400, None)
         else:
             return(response_json, 200, None)
 
 
-@app.route('/count_courses', methods=['POST', 'GET'])
-def get_or_post_courses():
+@app.route('/count_courses', methods=['GET'])
+def count_courses():
+    courses_collection = app.db.courses
+    result = courses_collection.count()
+    # pdb.set_trace()
+    return("Number of courses: {}".format(result), 200, None)
+
+
+@app.route('/carts', methods=['POST', 'PATCH', 'GET'])
+def get_post_or_patch_carts():
+    carts_collection = app.db.carts
+    prams = request.args
+    user = prams.get('user', type=str)
+    brand = prams.get('brand', type=str)
+    quantity = prams.get('quantity', type=int)
+    if request.method == 'GET':
+        if user is None:
+            result = carts_collection.find()
+        else:
+            result = carts_collection.find_one(
+                {'user': user}
+            )
+        response_json = dumps(result)
+        if response_json == 'null':
+            return('Error: There is no user named {}'.format(user), 400, None)
+        else:
+            return(response_json, 200, None)
+    elif request.method == 'POST':
+        carts_dict = request.json
+        carts_collection.insert_one(
+            carts_dict
+        )
+        json_result = JSONEncoder().encode(carts_dict)
+        return (json_result, 201, None)
+    elif request.method == 'PATCH':
+        if user is not None and brand is not None and quantity is not None:
+            result = carts_collection.find_one_and_update(
+                {'user': user, 'items.brand': brand},
+                {'$inc': {'items.$.quantity': quantity}},
+                return_document=ReturnDocument.AFTER
+            )
+            response_json = JSONEncoder().encode(result)
+            if response_json == 'null':
+                result = carts_collection.find_one_and_update(
+                    {'user': user},
+                    {'$addToSet': {"items": {'brand': brand, 'quantity': quantity}}},
+                    return_document=ReturnDocument.AFTER
+                )
+        response_json = JSONEncoder().encode(result)
+        if response_json == 'null':
+            return('Update failed', 400, None)
+        else:
+            return(response_json, 200, None)
 
 
 if __name__ == '__main__':
